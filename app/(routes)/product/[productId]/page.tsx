cc1625695bc7f5375c8b4f7899d839e4acb07348
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-08-02 16:35:36 +0100

    Feat: added info props

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,36 @@
-const ProductPage = () => {
-  return <div>Individual Product</div>
+import getProduct from "@/actions/get-product";
+import getProducts from "@/actions/get-products";
+import Gallery from "@/components/gallery";
+import Info from "@/components/info";
+import Container from "@/components/ui/container";
+import { FC } from "react";
+
+interface ProductPagePrps {
+  params: {
+    productId: string;
+  };
 }
 
-export default ProductPage
+const ProductPage: FC<ProductPagePrps> = async ({ params }) => {
+  const product = await getProduct(params.productId);
+  const suggestedProducts = await getProducts({
+    categoryId: product?.category?.id,
+  });
+
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            <Gallery images={product.images}/>
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <Info data={product}/>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default ProductPage;
--- components/info.tsx
+++ components/info.tsx
@@ -1,4 +1,11 @@
-const Info = () => {
+import { Product } from "@/types"
+import { FC } from "react"
+
+interface InfoProps {
+  data: Product;
+}
+
+const Info: FC<InfoProps> = ({data}) => {
   return <div>Info</div>
 }
 
