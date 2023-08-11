fae838333616a79eab8cc74d87380720010cc3a0
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-08-11 10:46:31 +0100

    Feat: filters and render at all views, except mobile, concluded

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -7,6 +7,8 @@ import getSizes from "@/actions/get-sizes";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
 import Filter from "./components/filter";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
 
 export const revalidate = 0;
 
@@ -44,6 +46,14 @@ const CategoryPage: FC<CategoryPageProps> = async ({
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
