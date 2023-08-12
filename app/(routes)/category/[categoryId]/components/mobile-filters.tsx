0a82d0eb1f8ff770f07bc03b54f752ec7d41bcbe
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-08-12 11:01:37 +0100

    Feat: finish category screen

--- app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import Button from "@/components/ui/button";
-import { Color, Size } from "@/types";
-import { Dialog } from "@headlessui/react";
-import { Plus } from "lucide-react";
 import { FC, useState } from "react";
+import { Plus, X } from "lucide-react";
+import { Dialog } from "@headlessui/react";
+
+import IconButton from "@/components/ui/icon-button";
+import { Color, Size } from "@/types";
+import Button from "@/components/ui/button";
+import Filter from "./filter";
 
 interface MobileFiltersProps {
   sizes: Size[];
@@ -31,12 +34,21 @@ const MobileFilters: FC<MobileFiltersProps> = ({ sizes, colors }) => {
         onClose={onClose}
       >
         {/* Background */}
-        <div className='fixed inset-0 bg-black bg-opacity-25' />
+        <div className="fixed inset-0 bg-black bg-opacity-25" />
 
         {/* Dialog position */}
-        <div className='fixed inset-0 z-40 flex'>
-          <Dialog.Panel>
-            
+        <div className="fixed inset-0 z-40 flex">
+          <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+            {/* Close Button */}
+            <div className="flex items-center justify-end px-4">
+              <IconButton icon={<X size={15} onClick={onClose} />} />
+            </div>
+
+            {/* Render the filters */}
+            <div className="p-4">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
           </Dialog.Panel>
         </div>
       </Dialog>
