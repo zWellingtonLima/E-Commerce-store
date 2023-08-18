f7e883e47a561d3491bba1590360c6431a782e00
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-08-18 10:03:12 +0100

    Feat: added router to redirect user to /cart after clicking on button

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,5 +1,6 @@
 import { ShoppingBag } from "lucide-react";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 import Button from "@/components/ui/button";
 import useCart from "@/hooks/use-cart";
@@ -11,6 +12,7 @@ const NavbarActions = () => {
     setIsMounted(true);
   }, []);
 
+  const router = useRouter();
   const cart = useCart();
 
   if (!isMounted) {
@@ -19,7 +21,7 @@ const NavbarActions = () => {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button onClick={() => router.push('/cart')} className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag size={20} color="white" />
         <span className="ml-2 text-sm font-medium text-white">
           {cart.items.length}
